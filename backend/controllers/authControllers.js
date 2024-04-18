8a1ee24ba916ee7f1883f7c5ae10a8cc5b9a6cb2
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-18 23:10:35 +0530

    sending jwt token in cookie

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,18 +1,14 @@
 import catchAsyncErrors from "../middlewares/catchAsyncError.js";
 import User from "../models/user.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import sendToken from "../utils/sendToken.js";
 
 //Register User =>/api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) =>{
     const {name, email, password} = req.body;
 
     const user = await User.create({ name, email, password});
-
-    const token = user.getJwtToken();
-    
-    res.status(201).json({
-        token
-    });
+    sendToken(user,201,res);
 });
 
 //Login User =>/api/v1/login
@@ -30,13 +26,9 @@ export const loginUser = catchAsyncErrors(async (req, res, next) =>{
     }
     //check if password is correct 
     const isPasswordMatched = await user.comparePassword(password);
-    console.log(isPasswordMatched,'passwird match');
+    
     if(!isPasswordMatched){
         return next(new ErrorHandler("Invalid email or password",401));
     }
-    const token = user.getJwtToken();
-    
-    res.status(200).json({
-        token
-    });
+    sendToken(user,200,res);
 });
