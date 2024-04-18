3cc71687ea5be237abdb2659ec83d3e14933833c
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-18 22:30:30 +0530

    creating jwt token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -6,7 +6,9 @@ export const registerUser = catchAsyncErrors(async (req, res, next) =>{
 
     const user = await User.create({ name, email, password});
 
+    const token = user.getJwtToken();
+    
     res.status(201).json({
-        success:true,
+        token
     });
 });
