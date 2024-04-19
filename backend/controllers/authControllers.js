14093c85218fdb33f014d0f29d2f6129e336df0a
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-19 20:18:55 +0530

    logout user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -32,3 +32,14 @@ export const loginUser = catchAsyncErrors(async (req, res, next) =>{
     }
     sendToken(user,200,res);
 });
+
+//logout user ---> /api/v1/logout
+export const logout = catchAsyncErrors(async (req, res, next) =>{
+    res.cookie("token",null, {
+        expires : new Date(Date.now()),
+        httpOnly : true
+    });
+    res.status(200).json({
+        message:"Logged Out"
+    })
+})
--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { registerUser,loginUser } from "../controllers/authControllers.js";
+import { registerUser,loginUser, logout } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 // localhost:4000/api/v1/register
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-
+router.route("/logout").get(logout);
 
 export default router;
