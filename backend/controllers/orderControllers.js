b2714b9c47131fec7a17fac58ac7f2fb940eefcc
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-22 21:16:56 +0530

    get single order and get login user orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -31,3 +31,23 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
     })
 });
  
+//Get order details => /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(!order){
+        return next(new ErrorHandler("No order found with this id",404));
+    }
+    res.status(201).json({
+        order 
+    })
+});
+ 
+//Get current user orders => /api/v1/me/orders
+export const myorders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({user : req.user._id}).populate("user", "name email");
+
+    res.status(201).json({
+        orders 
+    })
+});
--- backend/routers/order.js
+++ backend/routers/order.js
@@ -1,14 +1,13 @@
 import express from "express";
-import { newOrder } from "../controllers/orderControllers.js";
+import { getOrderDetails, myorders, newOrder } from "../controllers/orderControllers.js";
 import { isAuthentictedUser,authorizeRoles } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 
 router.route("/orders/new").post(isAuthentictedUser,newOrder);
+router.route("/orders/:id").get(isAuthentictedUser,getOrderDetails);
+router.route("/me/orders").get(isAuthentictedUser,myorders);
 
-// router.route("/products/:id").get(getProductDetails);
-// router.route("/admin/products/:id").put(isAuthentictedUser,authorizeRoles('admin'), updateProduct);
-// router.route("/admin/products/:id").delete(isAuthentictedUser,authorizeRoles('admin'), deleteProduct);
 
 export default router;
