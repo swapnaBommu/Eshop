a84f03839e7a94a8a0f70ad11233dc436b146539
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-24 22:14:05 +0530

    fetch all products query

--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -1,1 +1,15 @@
-import {} from '@reduxjs/toolkit';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const productApi = createApi({
+    reducerPath:"productApi",
+    baseQuery:fetchBaseQuery({baseUrl: "api/v1"}),
+    endpoints:(builder) => ({
+        getProducts : builder.query({
+            query: (params) =>({
+                url:"/products",
+            }), 
+        }),
+    }),
+});
+
+export const {  useGetProductsQuery } = productApi;
