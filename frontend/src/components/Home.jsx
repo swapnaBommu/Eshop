c66b21568bc37338cff88b23853afcc1f53b72b5
swapnaBommu <swapnabommu8143@gmail.com>
2024-04-25 21:02:34 +0530

    fetch and render products for redux state

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,48 +1,23 @@
 import React from 'react'
 import MetaData from './layout/MetaData'
-
+import { useGetProductsQuery } from '../redux/api/productsApi';
+import ProductItem from "./product/productItem";
 const Home = () => {
+  const { data } = useGetProductsQuery();
+  
   return (
     <>
     <MetaData title={"Home"} />
-    <div class="row">
-      <div class="col-12 col-sm-6 col-md-12">
-        <h1 id="products_heading" class="text-secondary">Latest Products</h1>
+    <div className="row">
+      <div className="col-12 col-sm-6 col-md-12">
+        <h1 id="products_heading" className="text-secondary">Latest Products</h1>
 
-        <section id="products" class="mt-5">
-          <div class="row">
-            {/*<!-- Product Item 1 -->*/}
-            <div class="col-sm-12 col-md-6 col-lg-3 my-3">
-              <div class="card p-3 rounded">
-                <img
-                  class="card-img-top mx-auto"
-                  src="./images/default_product.png"
-                  alt=""
-                />
-                <div
-                  class="card-body ps-3 d-flex justify-content-center flex-column"
-                >
-                  <h5 class="card-title">
-                    <a href="">Product Name 1</a>
-                  </h5>
-                  <div class="ratings mt-auto d-flex">
-                    <div class="star-ratings">
-                      <i class="fa fa-star star-active"></i>
-                      <i class="fa fa-star star-active"></i>
-                      <i class="fa fa-star star-active"></i>
-                      <i class="fa fa-star star-active"></i>
-                      <i class="fa fa-star star-active"></i>
-                    </div>
-                    <span id="no_of_reviews" class="pt-2 ps-2"> (0) </span>
-                  </div>
-                  <p class="card-text mt-2">$100</p>
-                  <a href="" id="view_btn" class="btn btn-block">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/*<!-- End Product Item 1 -->*/}
+        <section id="products" className="mt-5">
+          <div className="row">
+            { data?.products?.map((product) => (
+                <ProductItem product = {product}/>
+            ))}
+           
           </div>
         </section>
       </div>
--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -1,8 +1,12 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const productApi = createApi({
+    //reducerPath/reducerName is the name of reducer it should pass in our store as reducer name
     reducerPath:"productApi",
+    //in baseQuery we need to setup our baseurl in BE
     baseQuery:fetchBaseQuery({baseUrl: "api/v1"}),
+    // define our endpoints, here we are fetching data we should use query function
+    //query: An endpoint definition that retrieves data
     endpoints:(builder) => ({
         getProducts : builder.query({
             query: (params) =>({
